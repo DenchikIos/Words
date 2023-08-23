43ab871ad3ee84403eb9422652589f5ad9196540
Denis IOS Developer <denis.poddubnyy.111@gmail.com>
2023-08-23 14:54:06 +0300

    Initial Commit

---
+++ Words/WordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsApp.swift
+//  Words
+//
+//  Created by User on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
